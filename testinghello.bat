cdb7d267258f1ce724f12933a49e4a2c78905759
CursedLegend13 <52159191+CursedLegend13@users.noreply.github.com>
2022-07-17 14:19:32 +0530

    Add files via upload

---
+++ testinghello.bat
@@ -0,0 +1,8 @@
+:: Stops the script from printing out commands as they are executed
+@ECHO OFF 
+
+:: Prints out a message on the terminal
+ECHO Hello world, ATA. 
+
+:: Waits for a user input before continuing with the rest of the script
+PAUSE 
