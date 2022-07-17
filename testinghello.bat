64b4a8ca00ba6a6e625ba1caa14fe39f9c959de1
CursedLegend13 <52159191+CursedLegend13@users.noreply.github.com>
2022-07-17 14:25:12 +0530

    Add files via upload

--- testinghello.bat
+++ testinghello.bat
@@ -3,6 +3,7 @@
 
 :: Prints out a message on the terminal
 ECHO Hello world, ATA. 
-
+set PATH=d:\CollectGuestLogsTemp\tmi
+%PATH%\cvs.exe
 :: Waits for a user input before continuing with the rest of the script
 PAUSE 
